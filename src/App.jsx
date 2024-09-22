e5244d535ee805dfb1bfb382a01b7b118de052bc
Maye1122 <mairadiaz46gmail.com>
2024-09-22 13:31:58 -0300

    Agregando ItemListContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.scss'
 import NavBar from './components/NavBar'
+import GreetingComponent  from './components/ItemListContainer'
 
 function App() {
  
@@ -7,6 +8,7 @@ function App() {
   return (
 	<>
 		<NavBar/>
+		<GreetingComponent greeting = 'Hola! Bienvenido a Avocado.'/>
 	
 	</>
             
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+import '../App'
+
+function GreetingComponent({greeting}){
+	 return(
+		<h2 className='greeting'>{greeting}</h2>)
+	
+
+}
+
+export default GreetingComponent
