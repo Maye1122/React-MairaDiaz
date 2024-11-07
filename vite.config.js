730bdeab60678825a915dc1c43231e5d4d1ec9dc
Maye1122 <mairadiaz46gmail.com>
2024-11-07 20:52:19 -0300

    +deploy

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	build: {
 		sourcemap: false,
+		outDir: 'build', // Cambiar el directorio de salida a 'build'
 	},
 	resolve: {
 		alias: {
 			'@': '/src',
 		},
 	},
-})
+});
